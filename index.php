eb459d34b2368484e44be1478896f71c66689426
mikicola <michela.colangelo@gmail.com>
2022-04-19 00:13:08 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dischi</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+    <div class="container">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/7/71/Spotify.png"
+        alt="Logo Spotify"/>
+
+      <!-- <select class="genre-selector">
+        <option selected>Seleziona genere</option>
+        <option value="Rock">Rock</option>
+        <option value="pop">pop</option>
+        <option value="metal">metal</option>
+      </select> -->
+    </div>
+  </header>
+
+  <main>
+    <div class="discs container">
+        <?php
+        // qui va la card disco
+        ?>
+    </div>
+  </main>
+</body>
+</html>
