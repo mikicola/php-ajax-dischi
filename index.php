5d056924c6e240c55206f0495b302e25d2e06a4f
mikicola <michela.colangelo@gmail.com>
2022-04-19 11:34:51 +0200

    aggiungo ciclo foreach per le card-disc

--- index.php
+++ index.php
@@ -8,27 +8,36 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<header>
+    <header>
     <div class="container">
-      <img
+        <img
         src="https://upload.wikimedia.org/wikipedia/commons/7/71/Spotify.png"
         alt="Logo Spotify"/>
 
-      <!-- <select class="genre-selector">
+        <!-- <select class="genre-selector">
         <option selected>Seleziona genere</option>
         <option value="Rock">Rock</option>
         <option value="pop">pop</option>
         <option value="metal">metal</option>
-      </select> -->
+        </select> -->
     </div>
-  </header>
+    </header>
 
-  <main>
+    <main>
     <div class="discs container">
         <?php
+        include_once __DIR__ . '/data-discs.php';
         // qui va la card disco
+        foreach ($arrDataDiscs as $dataDisc) {?>
+            <div class="card-disc">
+                <img src="<?= $dataDisc['poster']?>" alt="<?= $dataDisc['title']?>">
+                <h3><?= $dataDisc['title']?></h3>
+                <small><?= $dataDisc['author']?></small>
+                <strong><?= $dataDisc['year']?></strong>
+            </div> <?php
+        }
         ?>
     </div>
-  </main>
+    </main>
 </body>
 </html>
